425107b8bbefa952cf8ec9629fb43695da84f747
sabashvili <sabashvili.shota@gmail.com>
2023-04-25 00:36:58 +0400

    Set some futures

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -6,28 +6,28 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: "Jonas Schmedtmann",
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: "Jessica Davis",
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: "Steven Thomas Williams",
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: "Sarah Smith",
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
@@ -36,45 +36,58 @@ const account4 = {
 const accounts = [account1, account2, account3, account4];
 
 // Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
+const labelWelcome = document.querySelector(".welcome");
+const labelDate = document.querySelector(".date");
+const labelBalance = document.querySelector(".balance__value");
+const labelSumIn = document.querySelector(".summary__value--in");
+const labelSumOut = document.querySelector(".summary__value--out");
+const labelSumInterest = document.querySelector(".summary__value--interest");
+const labelTimer = document.querySelector(".timer");
 
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
+const containerApp = document.querySelector(".app");
+const containerMovements = document.querySelector(".movements");
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+const btnLogin = document.querySelector(".login__btn");
+const btnTransfer = document.querySelector(".form__btn--transfer");
+const btnLoan = document.querySelector(".form__btn--loan");
+const btnClose = document.querySelector(".form__btn--close");
+const btnSort = document.querySelector(".btn--sort");
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+const inputLoginUsername = document.querySelector(".login__input--user");
+const inputLoginPin = document.querySelector(".login__input--pin");
+const inputTransferTo = document.querySelector(".form__input--to");
+const inputTransferAmount = document.querySelector(".form__input--amount");
+const inputLoanAmount = document.querySelector(".form__input--loan-amount");
+const inputCloseUsername = document.querySelector(".form__input--user");
+const inputClosePin = document.querySelector(".form__input--pin");
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
 const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
+  ["USD", "United States dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
 ]);
 
+const date = new Date();
+const day = date.getDate();
+const mounth = date.getMonth() + 1;
+const year = date.getFullYear();
+const hour = date.getHours();
+const minute = date.getMinutes();
+const getDate = (unit) => `${unit < 10 ? `0${unit}` : `${unit}`}`;
+const fullDate = `${getDate(day)}/${getDate(mounth)}/${year}, ${getDate(hour)}:${getDate(minute)}`;
+labelDate.textContent = fullDate;
+
+let summaryValueIn = 0;
+let summaryValueOut = 0;
 
 const displayMovements = function (movements) {
   movements.forEach(function (mov, i) {
-    const type = mov > 0 ? "deposit" : "withdrawal"
+    const type = mov > 0 ? "deposit" : "withdrawal";
+    type == "deposit" ? (summaryValueIn += mov) : (summaryValueOut += mov * -1);
     const html = `
         <div class="movements__row">
           <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
@@ -82,16 +95,17 @@ const displayMovements = function (movements) {
           <div class="movements__value">${mov}€</div>
         </div>
     `;
-    containerMovements.insertAdjacentHTML("afterbegin", html)
+    containerMovements.insertAdjacentHTML("afterbegin", html);
 
-
+    labelSumIn.textContent = `${summaryValueIn}€`;
+    labelSumOut.textContent = `${summaryValueOut}€`;
+    labelBalance.textContent = `${summaryValueIn - summaryValueOut}€`;
   });
-}
+};
 
-displayMovements(account1.movements)
+displayMovements(account1.movements);
 
 /////////////////////////////////////////////////
-
 
 /* 
 Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
@@ -111,13 +125,12 @@ TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
-
 const checkDogs = function (dogsJulia, dogsKate) {
-  const dogsJuliaCorrected = dogsJulia.slice(1, 3)
-  const dogs = [...dogsJuliaCorrected, ...dogsKate]
+  const dogsJuliaCorrected = dogsJulia.slice(1, 3);
+  const dogs = [...dogsJuliaCorrected, ...dogsKate];
   dogs.forEach(function (dog, i) {
-    console.log(`Dog number ${i + 1} is ${dog >= 3 ? "an adult," : "still a puppy 🐶"} and is ${dog} years old`)
+    console.log(`Dog number ${i + 1} is ${dog >= 3 ? "an adult," : "still a puppy 🐶"} and is ${dog} years old`);
   });
-}
+};
 
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3])
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
