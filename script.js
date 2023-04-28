58a775075ab9778f5fbb815661ad76eb49bd5035
Shota Sabashvili <s.sabashvili@ggtc.ge>
2023-04-28 17:37:48 +0400

    Create sort function

--- script.js
+++ script.js
@@ -81,20 +81,35 @@ const getDate = (unit) => `${unit < 10 ? `0${unit}` : `${unit}`}`;
 const fullDate = `${getDate(day)}/${getDate(mounth)}/${year}, ${getDate(hour)}:${getDate(minute)}`;
 labelDate.textContent = fullDate;
 
-const displayMovements = function (movements) {
-  movements.forEach(function (mov, i) {
+const displayMovements = function (movements, sort = false) {
+  containerMovements.innerHTML = ""
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
     const html = `
         <div class="movements__row">
           <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
           <div class="movements__date">3 days ago</div>
-          <div class="movements__value">${mov}€</div>
+          <div class="movements__value">${Math.abs(mov)}€</div>
         </div>
     `;
     containerMovements.insertAdjacentHTML("afterbegin", html);
+
   });
 };
+
+
+
+const updateUI = function (acc) {
+  calcDisplaySummary(acc);
+  calcPrintBalance(acc);
+  displayMovements(acc.movements);
+}
+
+
 
 const createUsernames = function (accs) {
   accs.forEach((acc) => {
@@ -105,12 +120,11 @@ const createUsernames = function (accs) {
       .join("");
   });
 };
-
 createUsernames(accounts);
 
-const calcPrintBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance}€`;
+const calcPrintBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance}€`;
 };
 
 const calcDisplaySummary = function (acc) {
@@ -134,12 +148,73 @@ btnLogin.addEventListener("click", function (e) {
     labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(" ")[0]}`;
     containerApp.style.opacity = "1";
 
-    calcDisplaySummary(currentAccount);
-    calcPrintBalance(currentAccount.movements);
-    displayMovements(currentAccount.movements);
+    updateUI(currentAccount)
     inputLoginUsername.value = inputLoginPin.value = "";
     inputLoginPin.blur();
   }
 });
 
+
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value)
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount)
+  }
+  inputLoanAmount.value = ""
+})
+
+
+btnTransfer.addEventListener("click", function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value)
+  const receiverAcc = accounts.find((acc) => acc.username == inputTransferTo.value)
+  if (receiverAcc && receiverAcc?.username !== currentAccount.username && currentAccount.balance >= amount && amount > 0) {
+    currentAccount.movements.push(-amount)
+    receiverAcc.movements.push(amount)
+    updateUI(currentAccount)
+  }
+  inputTransferTo.value = inputTransferAmount.value = ""
+  inputTransferAmount.blur()
+})
+
+
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault()
+  const inputCloseAcc = inputCloseUsername.value;
+  const inputClosePincode = Number(inputClosePin.value);
+
+  if (inputCloseAcc == currentAccount.username && inputClosePincode == currentAccount.pin) {
+    const CloseAccIndex = accounts.findIndex((acc) => inputCloseAcc == acc.username)
+    accounts.splice(CloseAccIndex, 1)
+    containerApp.style.opacity = "0";
+  }
+  inputCloseUsername.value = ""
+  inputClosePin.value = ""
+})
+
+let sorted = false;
+
+btnSort.addEventListener("click", function () {
+  displayMovements(currentAccount.movements, !sorted)
+  sorted = !sorted
+})
+
+
 /////////////////////////////////////////////////
+
+// this is a nice title -> This Is a Nice Title
+
+const convertTitleCase = function (title) {
+  const titleArr = title.split(" ")
+  const finalTitle = titleArr.reduce((acc, text) => {
+    text.length >= 2 ? acc += text[0].toUpperCase() + text.slice(1) + " " : acc += text[0].toLowerCase() + " "
+    return acc
+  }, "")
+
+  console.log(finalTitle);
+  return titleArr
+}
+
+console.log(convertTitleCase("this is a nice title"));
